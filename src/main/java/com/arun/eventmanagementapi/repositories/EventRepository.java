0a80ddd517502070bcf505808cfd6a9139f04d96
Arun Reddy <arunreddy.kayathi@gmail.com>
2019-05-16 22:13:54 -0400

    Add pagination to custom finders

--- src/main/java/com/arun/eventmanagementapi/repositories/EventRepository.java
+++ src/main/java/com/arun/eventmanagementapi/repositories/EventRepository.java
@@ -1,14 +1,19 @@
 package com.arun.eventmanagementapi.repositories;
 
 import com.arun.eventmanagementapi.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.ZoneId;
+
 
 @Repository
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
-    List<Event> findByName(@Param("name") String name);
+    Page<Event> findByName(@Param("name") String name, Pageable pageable);
+
+    Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
 }
